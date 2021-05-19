988fbfa4d48235f8fa1dcd52b48c3755cdc8fff4
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2021-05-19 11:31:26 -0400

    rearrange this folder

--- R_code/project_logistics/setup_new_action.R
+++ R_code/project_logistics/setup_new_action.R
@@ -1,25 +1,25 @@
 # R script to create folders for a project
 
-action<-"Framework9"
+action<-"FW9_Rebuild_AM"
 
 # Setup directories for R_code
-dir.create(file.path("R_code",action), recursive=TRUE)
-dir.create(file.path("R_code",action,"data_extraction_processing"), recursive=TRUE)
+dir.create(file.path(action,"R_code"), recursive=TRUE)
+dir.create(file.path(action,"R_code","data_extraction_processing"), recursive=TRUE)
 
 
 # Setup directories for data
-dir.create(file.path("data_folder", "external",action),  recursive=TRUE)
-dir.create(file.path("data_folder", "main",action),  recursive=TRUE)
-dir.create(file.path("data_folder", "internal",action),  recursive=TRUE)
-dir.create(file.path("data_folder", "raw",action),  recursive=TRUE)
-dir.create(file.path("data_folder", "intermediate",action),  recursive=TRUE)
+dir.create(file.path(action,"data_folder", "external"),  recursive=TRUE)
+dir.create(file.path(action,"data_folder", "main"),  recursive=TRUE)
+dir.create(file.path(action,"data_folder", "internal"),  recursive=TRUE)
+dir.create(file.path(action,"data_folder", "raw"),  recursive=TRUE)
+dir.create(file.path(action,"data_folder", "intermediate"),  recursive=TRUE)
 
 # Setup directories for stata_code
-dir.create(file.path("stata_code", action),  recursive=TRUE)
-dir.create(file.path("stata_code", action, "data_extraction_processing"),  recursive=TRUE)
-dir.create(file.path("stata_code", action, "analysis"),  recursive=TRUE)
+dir.create(file.path(action,"stata_code"),  recursive=TRUE)
+dir.create(file.path(action,"stata_code", "data_extraction_processing"),  recursive=TRUE)
+dir.create(file.path(action,"stata_code", "analysis"),  recursive=TRUE)
 
 # Setup directories for tables, images, and results
-dir.create(file.path("tables", action),  recursive=TRUE)
-dir.create(file.path("images", action), recursive=TRUE)
-dir.create(file.path("results", action),recursive=TRUE)
+dir.create(file.path(action,"tables"),  recursive=TRUE)
+dir.create(file.path(action,"images"), recursive=TRUE)
+dir.create(file.path(action,"results"),recursive=TRUE)
