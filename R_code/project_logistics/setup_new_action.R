c76a421221441c0d7c7c0edc29b871f8022d5f80
mle2718 <Min-Yang.Lee@noaa.gov>
2022-09-30 11:58:35 -0400

    here()-ify

--- R_code/project_logistics/setup_new_action.R
+++ R_code/project_logistics/setup_new_action.R
@@ -1,25 +1,26 @@
 # R script to create folders for a project
+library(here)
+my_projdir<- here::i_am("R_code/project_logistics/setup_new_action.R")
 
-action<-"FW9_Rebuild_AM"
-
+action<-"SIR2023_2026"
 # Setup directories for R_code
-dir.create(file.path(action,"R_code"), recursive=TRUE)
-dir.create(file.path(action,"R_code","data_extraction_processing"), recursive=TRUE)
+dir.create(here(action,"R_code"), recursive=TRUE)
+dir.create(here(action,"R_code","data_extraction_processing"), recursive=TRUE)
 
 
 # Setup directories for data
-dir.create(file.path(action,"data_folder", "external"),  recursive=TRUE)
-dir.create(file.path(action,"data_folder", "main"),  recursive=TRUE)
-dir.create(file.path(action,"data_folder", "internal"),  recursive=TRUE)
-dir.create(file.path(action,"data_folder", "raw"),  recursive=TRUE)
-dir.create(file.path(action,"data_folder", "intermediate"),  recursive=TRUE)
+dir.create(here(action,"data_folder", "external"),  recursive=TRUE)
+dir.create(here(action,"data_folder", "main"),  recursive=TRUE)
+dir.create(here(action,"data_folder", "internal"),  recursive=TRUE)
+dir.create(here(action,"data_folder", "raw"),  recursive=TRUE)
+dir.create(here(action,"data_folder", "intermediate"),  recursive=TRUE)
 
 # Setup directories for stata_code
-dir.create(file.path(action,"stata_code"),  recursive=TRUE)
-dir.create(file.path(action,"stata_code", "data_extraction_processing"),  recursive=TRUE)
-dir.create(file.path(action,"stata_code", "analysis"),  recursive=TRUE)
+dir.create(here(action,"stata_code"),  recursive=TRUE)
+dir.create(here(action,"stata_code", "data_extraction_processing"),  recursive=TRUE)
+dir.create(here(action,"stata_code", "analysis"),  recursive=TRUE)
 
 # Setup directories for tables, images, and results
-dir.create(file.path(action,"tables"),  recursive=TRUE)
-dir.create(file.path(action,"images"), recursive=TRUE)
-dir.create(file.path(action,"results"),recursive=TRUE)
+dir.create(here(action,"tables"),  recursive=TRUE)
+dir.create(here(action,"images"), recursive=TRUE)
+dir.create(here(action,"results"),recursive=TRUE)
