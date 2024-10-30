37a316804e70ee4f4d778eda4cdbfc42c3ebf93e
mle2718 <Min-Yang.Lee@noaa.gov>
2024-10-30 14:46:30 -0400

    pass in list of species. remove catch data readin and join. classify in domain using case_when(). Save Rds

--- Amendment10/R_code/data_extraction_processing/processing/01_MRIP_Targeting.R
+++ Amendment10/R_code/data_extraction_processing/processing/01_MRIP_Targeting.R
@@ -23,15 +23,15 @@ options(scipen=999)
 options(survey.lonely.psu = "certainty")
 
 
-# running local 
 local_BLAST_folder<-file.path("V:","READ-SSB-Lee-MRIP-BLAST")
-network_BLAST_folder<-file.path("blast","READ-SSB-Lee-MRIP-BLAST")
+network_BLAST_folder<-file.path("//nefscfile","BLAST" ,"READ-SSB-Lee-MRIP-BLAST")
 
-
+# run local
 raw_mrip_folder<-file.path(local_BLAST_folder,"data_folder","raw")
+# run network
+raw_mrip_folder<-file.path(network_BLAST_folder,"data_folder","raw")
 
-
-species1<-"stripedbass"
+specieslist<-c("stripedbass","blackseabass")
 ################################################################################
 ################################################################################
 ########################################SET UPS ################################
@@ -42,7 +42,7 @@ sizeb2_list<-list.files(path=raw_mrip_folder, pattern=glob2rx("size_b2_2*.Rds"))
 catch_list<-list.files(path=raw_mrip_folder, pattern=glob2rx("catch_*.Rds"))
 trip_list<-list.files(path=raw_mrip_folder, pattern=glob2rx("trip_*.Rds"))
 
-year<-as.character(2021:2023)
+year<-as.character(2020:2024)
 year<-as.data.frame(year)
 waves<-as.character(1:6)
 waves<-as.data.frame(waves)
@@ -57,8 +57,9 @@ readins<-as.list(readins)
 
 
 
-ma_allocation<-haven::read_dta(file.path(raw_mrip_folder,"ma_site_allocation.dta"))
+ma_site_allocation<-haven::read_dta(file.path(raw_mrip_folder,"ma_site_allocation.dta"))
 
+names(ma_site_allocation) <- tolower(names(ma_site_allocation))
 
 
 
@@ -165,85 +166,43 @@ trip_dataset<-trip_dataset %>%
 
 
 trip_dataset<-trip_dataset %>%
-  left_join(ma_allocation, by=join_by(intsite==site_id))
-
-
-
-######READ in Trip Dataset####################
-catch_dataset<-lapply(readins,readin_catch)
-catch_dataset<-rbindlist(catch_dataset, fill=TRUE)
-
-# column names to lower case, fill na., 
-# convert contents of common to lower case and remove all white space 
-catch_dataset<-catch_dataset %>%
-  rename_all(tolower) %>%
-  mutate(var_id=ifelse(var_id=="",strat_id,var_id),
-         wp_catch=ifelse(is.na(wp_catch),wp_int, wp_catch),
-         claim=ifelse(is.na(claim),0,claim),
-         wp_int=ifelse(wp_int<0,0,wp_int)) %>%
-  mutate(common=str_to_lower(common))%>%
-  mutate(common=str_replace_all(common, pattern=" ", ""))
-
-# Merge trip to catch. For columns in both dataset, column from the "trip" dataset          
-trip_dataset<-trip_dataset %>%
-  dplyr::full_join(catch_dataset, by=join_by(year, strat_id, psu_id, id_code), relationship="one-to-many", suffix=c("",".y")) %>%
-  select(-ends_with(".y"))
-
+  left_join(ma_site_allocation, by=join_by(intsite==site_id))
+rows_of_trip<-nrow(trip_dataset)
 
 trip_dataset<-trip_dataset %>%
   mutate(mode=ifelse(mode_fx==4,"FH",ifelse(mode_fx==5,"FH","PR"))
 )
 
-trip_dataset$dom_id<-2
-
-# This is trips that caught species1
-# trip_dataset$dom_id[trip_dataset$common ==species1 ]<-1
 
 
-# This is trips that targeted species1
-trip_dataset$dom_id[trip_dataset$prim1_common ==species1 ]<-1
-trip_dataset$dom_id[trip_dataset$prim2_common ==species1 ]<-1
+# Classify rows in "trip" that are in my domain. At the end of this code, I need the same number of rows as the original trip dataset
 
+# rows_of_trip<-nrow(trip_dataset)) 
+#   group_by(year, wave, strat_id, psu_id, id_code)%>%
 
+#create a categorical for whether the row caught or targeted species in specieslist
 
-# Deal with Group Catch 
-# For group catch, we want to keep only the largest value of claim from the leader.  
-# I am not sure if this matters for targeting.  
-
-# -- this bit of code generates a flag for each year-strat_id psu_id leader. (equal to the lowest of the dom_id)
-
-# 
-# 
-# Flag the year-wave-strat_id, psu_id, leader. Pull in the dom_id.
 trip_dataset<-trip_dataset %>%
-  group_by(year, wave, strat_id, psu_id, leader)%>%
-  arrange(year, wave, strat_id, psu_id, dom_id)%>%
-  mutate(gc_flag=dplyr::first(dom_id))
+  mutate(targeted=case_when(prim1_common %in% specieslist ~1, .default=0)
+  ) %>%
+  mutate(targeted=case_when(prim2_common %in% specieslist ~1, .default=targeted)
+)
 
 
-# Generates a flag for claim equal to the largest claim.
-# [This is probably wrong. it's pulling forward the largest value of claim, regardless of species
+# sum those variables, so they are constant within each year, wave, strat_id, psu_id, id_code. Pick of the first row.
 trip_dataset<-trip_dataset %>%
-  group_by(year, wave, strat_id, psu_id, leader)%>%
-  arrange(year, wave,strat_id, psu_id, claim)%>%
-  mutate(claim_flag=dplyr::last(claim))
+  group_by(year, wave, strat_id, psu_id, id_code)%>%
+  mutate(targeted=sum(targeted)) %>%
+  slice_head(n=1) %>%
+  ungroup()
 
-# Re-classifies the trip into dom_id=1 if that trip had catch of species in dom_id1
-trip_dataset$dom_id[(trip_dataset$dom_id ==2) &
-  (trip_dataset$claim_flag >0) & is.na(trip_dataset$claim_flag)==FALSE  &
-   trip_dataset$gc_flag==1]<-1
+# Classify as in my domain if caught or targeted
+trip_dataset<-trip_dataset %>%
+ mutate(dom_id=case_when(targeted>=1 ~ 1, .default=0) 
+         )
 
-
-
-# keep 1 observation per year-strat-psu-id_code. This will have dom_id=1 if it
-# targeted or caught my_common1 or my_common2. Else it will be dom_id=2*/
-# bysort year wave strat_id psu_id id_code (dom_id): gen count_obs1=_n
-# 
-# keep if count_obs1==1
-
-  
-
-# # You might want to adjust this by filtering: just new England or just ME, MA, NH.  
+# # You might want to adjust this by filtering: just new England or just ME, MA, NH.
+# Be careful where you put this piece of code
 # trip_dataset$dom_id[!trip_dataset$sub_reg %in% c(4)]<-2
 # trip_dataset$dom_id[!trip_dataset$st %in% c(23,33,25)]<-2
 # # Use the ma_site_allocation to classify intercept sites into GOM and not?
@@ -252,15 +211,10 @@ trip_dataset$dom_id[(trip_dataset$dom_id ==2) &
 
 
 
- trip_dataset<-trip_dataset %>%
-   group_by(year, wave, strat_id, psu_id, id_code)%>%
-   arrange(year, wave, strat_id, psu_id, id_code, dom_id)%>%
-   slice_head(n=1) %>%
-   dplyr::filter(dom_id==1)
 
- # Drop protest st_res fips codes
-trip_dataset<-trip_dataset %>%
-  dplyr::filter(st_res<99)
+# Drop protest st_res fips codes
+# trip_dataset<-trip_dataset %>%
+#   dplyr::filter(st_res<99)
  
 # Targeting 
 # srvyr data prep
@@ -283,16 +237,28 @@ tidy_trips_in<-trip_dataset %>%
 #   )
 # 
 # 
-# target_totals_stco<-tidy_trips_in %>%
-#   dplyr::filter(dom_id==1) %>%
-#   group_by(year, wave,stco ) %>%
-#   summarise(dtrip=round(survey_total(dtrip))
-#)
+ target_totals_stco<-tidy_trips_in %>%
+   dplyr::filter(dom_id==1) %>%
+   group_by(year, wave,stco ) %>%
+   summarise(dtrip=round(survey_total(dtrip))
+)
 
+ target_totals_stco$target<-paste(specieslist, collapse = "_")
+ 
+ 
 target_totals_zip<-tidy_trips_in %>%
   dplyr::filter(dom_id==1) %>%
   group_by(st_res, zip ) %>%
   summarise(dtrip=round(survey_total(dtrip))
   )
+
+target_totals_zip$target<-paste(specieslist, collapse = "_")
+
+
+
+saveRDS(target_totals_stco,file=here("data_folder", "main", paste0("targeted_stco_",vintage_string,".Rds")))
+
+
+saveRDS(target_totals_zip,file=here("data_folder", "main", paste0("targeted_zip_",vintage_string,".Rds")))
 
 
